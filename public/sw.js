41f642bbf3f4b481f5e3a2cf8336bd6a73cd6663
yimoyuanwei <1144212759@qq.com>
2026-07-16 20:15:21 +0800

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'money-tracker-v2';
+const ASSETS = [
+  '/',
+  '/standalone.html',
+  '/manifest.json',
+  '/icons/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
